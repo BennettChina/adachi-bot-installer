61dc2e5cf55a5a0e228ddf2df0b147832cad6ac4
bennett <w2500513053@outlook.com>
2022-06-19 18:14:17 +0800

    - 安卓版脚本兼容更多系统 - 重命名脚本文件用来区分

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # adachi-bot-installer
+
 [SilveryStar/Adachi-BOT](https://github.com/SilveryStar/Adachi-BOT)的一键部署脚本
 
 ## Linux
 
-`Linux`环境可以直接执行下面的命令，将通过安装`Docker`、`docker-compose`的方式运行BOT
+`Linux`环境可以直接执行下面的命令，将通过安装`Docker`、`docker-compose`的方式运行BOT。不想使用 `Docker` 启动的方式可以使用 [安装BOT的脚本](#安装BOT的脚本)
+这个脚本安装，这个脚本将在物理机安装 `nodejs`、`redis`、`chrome`等
 
 ```sh
-wget https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/adachi_bot_install.sh -O adachi_bot_install.sh && sudo bash adachi_bot_install.sh && rm adachi_bot_install.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/unix-docker-install.sh -O unix-docker-install.sh && sudo bash unix-docker-install.sh && rm unix-docker-install.sh
 ```
 
 ## macOS
@@ -14,7 +16,7 @@ wget https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-b
 `macOS`需要自行安装`docker`(最好是比较新的版本，因为自带了`docker-compose`)，[docker下载地址](https://www.docker.com/get-started) 。
 
 ```shell
-curl -L -# https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/adachi_bot_install.sh -o adachi_bot_install.sh && bash adachi_bot_install.sh && rm adachi_bot_install.sh
+curl -L -# https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/unix-docker-install.sh -o unix-docker-install.sh && bash unix-docker-install.sh && rm unix-docker-install.sh
 ```
 
 ## Windows
@@ -29,16 +31,20 @@ curl -L -# https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/ad
 - 系统禁止运行脚本，可使用 `Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force` 然后在执行命令。
 
 ```powershell
-iwr "https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/adachi_bot_install.ps1" -O .\adachi_bot_install.ps1; .\adachi_bot_install.ps1; rm .\adachi_bot_install.ps1
+iwr "https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/win-install.ps1" -O .\adachi_bot_install.ps1; .\adachi_bot_install.ps1; rm .\adachi_bot_install.ps1
 ```
 
 ## Android
 
-使用 `termux` [App](https://github.com/termux/termux-app) 来安装(可以通过 `F-Droid` 来[下载](https://f-droid.org/en/packages/com.termux/) 安装)，参考[云崽BOT](https://github.com/Le-niao/Yunzai-Bot) 的安卓安装方式。
+使用 `termux` [App](https://github.com/termux/termux-app) 来安装(可以通过 `F-Droid`
+来[下载](https://f-droid.org/en/packages/com.termux/) 安装)，参考[云崽BOT](https://github.com/Le-niao/Yunzai-Bot) 的安卓安装方式。
 
 下载完 `termux` 后执行 `pkg install proot git python -y`
 
-使用[国光大佬](https://github.com/sqlsec/termux-install-linux) 提供的Linux系统安装脚本安装一个Linux系统（虽然 `termux` 自身就是Linux环境，但毕竟是模拟的，还是需要真正的Linux环境才能安装上BOT ）
+### 安装Linux系统
+
+使用[国光大佬](https://github.com/sqlsec/termux-install-linux) 提供的Linux系统安装脚本安装一个Linux系统（虽然 `termux`
+自身就是Linux环境，但毕竟是模拟的，还是需要真正的Linux环境才能安装上BOT ）
 
 ```shell
 git clone https://ghproxy.com/https://github.com/sqlsec/termux-install-linux
@@ -53,12 +59,14 @@ cd ~/Termux-Linux/Ubuntu
 ./start-ubuntu.sh
 ```
 
-启动系统后再执行下面的命令(脚本除已选择的配置其他均使用的都是官方的默认配置，可根据 [官方文档](https://docs.adachi.top/config/) 自定义配置)
+### 安装BOT的脚本
+
+启动系统后再执行下面的命令(脚本暂时只支持 `Ubuntu`、`Debian`、`Centos`，除已选择的配置其他均使用的都是官方的默认配置，可根据 [官方文档](https://docs.adachi.top/config/)
+自定义配置)
 
 ```shell
-wget https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/termux-ubuntu.sh -O termux-ubuntu.sh && sudo bash termux-ubuntu.sh && rm termux-ubuntu.sh
+wget https://ghproxy.com/https://raw.githubusercontent.com/BennettChina/adachi-bot-installer/main/linux-install.sh -O linux-install.sh && sudo bash linux-install.sh && rm linux-install.sh
 ```
-
 
 ## 感谢
 
---
+++ linux-install.sh
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+ask() {
+  char_count='0'
+  prompt="${1}: "
+  reply=''
+  while IFS='' read -n '1' -p "${prompt}" -r -s 'char'; do
+    case "${char}" in
+    # Handles NULL
+    $'\000')
+      break
+      ;;
+      # Handles BACKSPACE and DELETE
+    $'\010' | $'\177')
+      if ((char_count > 0)); then
+        prompt=$'\b \b'
+        reply="${reply%?}"
+        ((char_count--))
+      else
+        prompt=''
+      fi
+      ;;
+    *)
+      prompt='*'
+      reply+="${char}"
+      ((char_count++))
+      ;;
+    esac
+  done
+  printf '\n' >&2
+  printf '%s\n' "${reply}"
+}
+
+os=$(cat /etc/*release | grep ^NAME | tr -d 'NAME="') >/dev/null 2>&1
+# 安装nodejs
+echo '安装nodejs开始'
+if ! type node >/dev/null 2>&1; then
+  if [ "$os" == "Ubuntu" ] || [ "$os" == "Debian" ]; then
+    wget https://deb.nodesource.com/setup_14.x -O - | bash -
+    apt-get install -y nodejs
+  elif [ "$os" == "Centos" ]; then
+    wget https://rpm.nodesource.com/setup_14.x -O - | bash -
+    yum install -y nodejs
+  fi
+  npm config set registry https://registry.npmmirror.com
+else
+  echo 'nodejs已安装'
+fi
+echo '安装nodejs完成'
+
+# 安装chromium
+echo '安装redis开始'
+if [ "$os" == "Ubuntu" ] || [ "$os" == "Debian" ]; then
+  apt install chromium-browser -y
+elif [ "$os" == "Centos" ]; then
+  yum install -y chromium
+fi
+echo '安装chromium完成'
+
+# 安装中文字体
+echo '安装中文字体开始'
+if [ "$os" == "Ubuntu" ] || [ "$os" == "Debian" ]; then
+  apt install -y --force-yes --no-install-recommends fonts-wqy-microhei
+elif [ "$os" == "Centos" ]; then
+  yum -y install wqy-microhei-fonts
+fi
+echo '安装中文字体完成'
+
+# 安装git
+echo '安装git开始'
+if [ "$os" == "Ubuntu" ] || [ "$os" == "Debian" ]; then
+  apt install git -y
+elif [ "$os" == "Centos" ]; then
+  yum -y install git
+fi
+echo '安装git完成'
+
+# 克隆项目
+echo '克隆Adachi-BOT开始'
+if [ ! -d "Adachi-BOT/" ]; then
+  git clone https://ghproxy.com/https://github.com/SilveryStar/Adachi-BOT.git --depth=1
+else
+  echo '项目已存在'
+fi
+
+cd Adachi-BOT || {
+  echo "克隆项目失败"
+  exit 1
+}
+work_dir=$(pwd)
+
+# 安装并运行redis
+echo '安装redis开始'
+apt-get install redis -y
+mv "/etc/redis/redis.conf" "/etc/redis/redis.conf.bak"
+database="${work_dir}/database"
+cp "redis.conf" "/etc/redis/redis.conf"
+if [ ! -d "${database}" ]; then
+  mkdir -p "${database}"
+fi
+sed -i "s|dir /data/|dir ${database}|" "/etc/redis/redis.conf"
+echo "daemonize yes" >>"/etc/redis/redis.conf"
+redis-server /etc/redis/redis.conf
+echo '安装redis完成'
+
+echo "开始选择安装插件，回复编号选择(回复all选择全部,回复0结束选择)..."
+cd "src/plugins" || {
+  echo "BOT项目结构发生变化或者未完整克隆，可在GitHub中提交issue提醒脚本作者更新!"
+  exit 1
+}
+use_plugins=""
+select plugin in "音乐插件" "抽卡分析" "圣遗物评分" "聊天插件" "搜图插件" "设置入群欢迎词插件" "热点新闻订阅插件"; do
+  case $plugin in
+  "音乐插件")
+    git clone -b music https://ghproxy.com/https://github.com/SilveryStar/Adachi-Plugin.git --depth=1 music
+    use_plugins="${use_plugins} ""[音乐插件]"
+    echo "音乐插件已下载，使用方式请访问 https://github.com/SilveryStar/Adachi-Plugin/tree/music"
+    ;;
+  "抽卡分析")
+    git clone https://ghproxy.com/https://github.com/wickedll/genshin_draw_analysis.git --depth=1
+    use_plugins="${use_plugins}"" [抽卡分析插件]"
+    echo "抽卡分析插件已下载，使用方式请访问 https://github.com/wickedll/genshin_draw_analysis"
+    ;;
+  "圣遗物评分")
+    git clone https://ghproxy.com/https://github.com/wickedll/genshin_rating.git --depth=1
+    use_plugins="${use_plugins} "" [圣遗物评分插件]"
+    echo "圣遗物评分插件已下载，使用方式请访问 https://github.com/wickedll/genshin_rating"
+    ;;
+  "聊天插件")
+    git clone https://ghproxy.com/https://github.com/Extrwave/Chat-Plugins.git --depth=1
+    use_plugins="${use_plugins} "" [聊天插件]"
+    echo "聊天插件已下载，使用方式请访问 https://github.com/Extrwave/Chat-Plugins"
+    ;;
+  "搜图插件")
+    git clone https://ghproxy.com/https://github.com/MarryDream/pic_search.git --depth=1
+    use_plugins="${use_plugins} "" [搜图插件]"
+    echo "搜图插件已下载，使用方式请访问 https://github.com/MarryDream/pic_search"
+    ;;
+  "设置入群欢迎词插件")
+    git clone https://ghproxy.com/https://github.com/BennettChina/group_helper.git --depth=1
+    use_plugins="${use_plugins} "" [设置入群欢迎词插件]"
+    echo "设置入群欢迎词插件已下载，使用方式请访问 https://github.com/BennettChina/group_helper"
+    ;;
+  "热点新闻订阅插件")
+    git clone https://ghproxy.com/https://github.com/BennettChina/hot-news.git --depth=1
+    use_plugins="${use_plugins} "" [热点新闻订阅插件]"
+    echo "热点新闻订阅插件已下载，使用方式请访问 https://github.com/BennettChina/hot-news"
+    ;;
+  "all")
+    git clone -b music https://ghproxy.com/https://github.com/SilveryStar/Adachi-Plugin.git --depth=1 music
+    echo "音乐插件已下载，使用方式请访问 https://github.com/SilveryStar/Adachi-Plugin/tree/music"
+    git clone https://ghproxy.com/https://github.com/wickedll/genshin_draw_analysis.git --depth=1
+    echo "抽卡分析插件已下载，使用方式请访问 https://github.com/wickedll/genshin_draw_analysis"
+    git clone https://ghproxy.com/https://github.com/wickedll/genshin_rating.git --depth=1
+    echo "圣遗物评分插件已下载，使用方式请访问 https://github.com/wickedll/genshin_rating"
+    git clone https://ghproxy.com/https://github.com/Extrwave/Chat-Plugins.git --depth=1
+    echo "聊天插件已下载，使用方式请访问 https://github.com/Extrwave/Chat-Plugins"
+    git clone https://ghproxy.com/https://github.com/MarryDream/pic_search.git --depth=1
+    echo "搜图插件已下载，使用方式请访问 https://github.com/MarryDream/pic_search"
+    git clone https://ghproxy.com/https://github.com/BennettChina/group_helper.git --depth=1
+    echo "设置入群欢迎词插件已下载，使用方式请访问 https://github.com/BennettChina/group_helper"
+    git clone https://ghproxy.com/https://github.com/BennettChina/hot-news.git --depth=1
+    echo "热点新闻订阅插件已下载，使用方式请访问 https://github.com/BennettChina/hot-news"
+    echo "已为你下载全部插件!"
+    break
+    ;;
+  *)
+    if [ "${use_plugins}" ]; then
+      echo "插件选择结束，你选择了${use_plugins}"
+      break
+    fi
+    echo "插件选择结束，你未选择插件!"
+    break
+    ;;
+  esac
+done
+cd "${work_dir}" || {
+  echo "插件安装完成，退出插件目录失败"
+  exit 1
+}
+
+echo "开始创建配置文件..."
+if [ ! -d "${work_dir}/config" ]; then mkdir -p "${work_dir}/config"; fi
+cd "${work_dir}/config" && touch setting.yml commands.yml cookies.yml genshin.yml && (cd "${work_dir}" || {
+  echo "退出配置目录失败"
+  exit 1
+})
+
+echo "请选择机器人登录平台(输入编号):"
+select platform_str in "安卓手机" "安卓Pad" "安卓手表" "MacOS" "iPad"; do
+  case $platform_str in
+  "安卓手机")
+    platform=1
+    break
+    ;;
+  "安卓Pad")
+    platform=2
+    break
+    ;;
+  "安卓手表")
+    platform=3
+    break
+    ;;
+  "MacOS")
+    platform=4
+    break
+    ;;
+  "iPad")
+    platform=5
+    break
+    ;;
+  *)
+    echo "你选择的登录平台编号非法."
+    ;;
+  esac
+done
+read -p "请输入机器人的QQ号: " qq_num
+echo "请选择登录方式:"
+select login_type in "密码" "扫码"; do
+  if [ $login_type == "密码" ]; then
+    qq_password="$(ask Password)"
+    qrcode=false
+  else
+    qrcode=true
+    qq_password='""'
+  fi
+  break
+done
+read -p "请输入机器人主人账号: " master_num
+echo '获取米游社cookie方式:
+将下面的代码复制并添加到一个书签中，书签名称自定义。然后在已登录的米游社网页中点击刚才的书签即可将cookie复制到剪切板中.
+javascript:(function () {let domain = document.domain;let cookie = document.cookie;const text = document.createElement("textarea");text.hidden=true;text.value = cookie;document.body.appendChild(text);text.select();text.setSelectionRange(0, 99999);navigator.clipboard.writeText(text.value).then(()=>{alert("domain:"+domain+"\ncookie is in clipboard");});document.body.removeChild(text);})();
+'
+read -p "请输入一个米游社cookie: " mys_cookie
+
+jwt_secret="$(tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 16 | head -n 1)"
+
+echo "qrcode: ${qrcode}
+number: ${qq_num}
+password: ${qq_password}
+master: ${master_num}
+header: \"#\"
+platform: ${platform}
+atUser: false
+inviteAuth: master
+countThreshold: 60
+groupIntervalTime: 1500
+privateIntervalTime: 2000
+helpMessageStyle: message
+logLevel: info
+dbPort: 56379
+dbPassword: \"\"
+webConsole:
+  enable: false
+  consolePort: 80
+  tcpLoggerPort: 54921
+  jwtSecret: ${jwt_secret}
+atBOT: false
+addFriend: true" >"${work_dir}/config/setting.yml"
+
+echo "cookies:
+  - ${mys_cookie}" >"${work_dir}/config/cookies.yml"
+
+echo "cardWeaponStyle: normal
+cardProfile: random
+serverPort: 58612" >"${work_dir}/config/genshin.yml"
+
+echo "正在为您安装依赖..."
+npm i
+npm i pm2 -g
+echo "依赖已完成安装，将为您启动服务..."
+npm start
+pm2 log
